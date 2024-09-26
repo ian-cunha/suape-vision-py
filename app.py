0b50326811e02e55d7b364a7810ba62fb7aaf679
Ian <iancunha20@gmail.com>
2024-09-26 00:44:49 -0300

    update

--- app.py
+++ app.py
@@ -7,45 +7,109 @@ from datetime import datetime
 app = Flask(__name__)
 
 dados = [
-    {'id': 'QW12E', 'categoria': 'New Panamax', 'entrada': '0', 'saida': '0', 'dock': 'A1', 'data': '2024-09-15'},
-    {'id': 'RT56Y', 'categoria': 'Ultra Large Container Ship', 'entrada': '0', 'saida': '0', 'dock': 'A2', 'data': '2024-07-22'},
-    {'id': 'YU78H', 'categoria': 'Post-Panamax', 'entrada': '0', 'saida': '0', 'dock': 'A3', 'data': '2024-06-30'},
-    {'id': 'DF34G', 'categoria': 'Container Ship', 'entrada': '0', 'saida': '0', 'dock': 'A4', 'data': '2024-05-10'},
-    {'id': 'GH89J', 'categoria': 'Feeder Ship', 'entrada': '0', 'saida': '0', 'dock': 'A1', 'data': '2024-04-20'},
-    {'id': 'JK12M', 'categoria': 'General Cargo Ship', 'entrada': '0', 'saida': '0', 'dock': 'A2', 'data': '2024-03-05'},
-    {'id': 'LP34N', 'categoria': 'Bulk Carer', 'entrada': '0', 'saida': '0', 'dock': 'A3', 'data': '2024-02-14'},
-    {'id': 'ZX56O', 'categoria': 'Roll-on/Rollri-off', 'entrada': '0', 'saida': '0', 'dock': 'A4', 'data': '2024-01-01'},
+    {
+        "id": "QW12E",
+        "categoria": "New Panamax",
+        "entrada": "0",
+        "saida": "0",
+        "dock": "A1",
+        "data": "2024-09-15",
+    },
+    {
+        "id": "RT56Y",
+        "categoria": "Ultra Large Container Ship",
+        "entrada": "0",
+        "saida": "0",
+        "dock": "A2",
+        "data": "2024-07-22",
+    },
+    {
+        "id": "YU78H",
+        "categoria": "Post-Panamax",
+        "entrada": "0",
+        "saida": "0",
+        "dock": "A3",
+        "data": "2024-06-30",
+    },
+    {
+        "id": "DF34G",
+        "categoria": "Container Ship",
+        "entrada": "0",
+        "saida": "0",
+        "dock": "A4",
+        "data": "2024-05-10",
+    },
+    {
+        "id": "GH89J",
+        "categoria": "Feeder Ship",
+        "entrada": "0",
+        "saida": "0",
+        "dock": "A1",
+        "data": "2024-04-20",
+    },
+    {
+        "id": "JK12M",
+        "categoria": "General Cargo Ship",
+        "entrada": "0",
+        "saida": "0",
+        "dock": "A2",
+        "data": "2024-03-05",
+    },
+    {
+        "id": "LP34N",
+        "categoria": "Bulk Carer",
+        "entrada": "0",
+        "saida": "0",
+        "dock": "A3",
+        "data": "2024-02-14",
+    },
+    {
+        "id": "ZX56O",
+        "categoria": "Roll-on/Rollri-off",
+        "entrada": "0",
+        "saida": "0",
+        "dock": "A4",
+        "data": "2024-01-01",
+    },
 ]
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    video_data = {
-        'title': 'CAM-01',
-        'url': 'vxqQyW4b9jA'
-    }
-    return render_template('index.html', video=video_data, planilha=dados)
+    video_data = {"title": "CAM-01", "url": "vxqQyW4b9jA"}
+    return render_template("index.html", video=video_data, planilha=dados)
 
-@app.route('/download_csv')
+
+@app.route("/download_csv")
 def download_csv():
     output = io.StringIO()
-    writer = csv.DictWriter(output, fieldnames=['id', 'categoria', 'entrada', 'saida', 'dock', 'data'])
+    writer = csv.DictWriter(
+        output, fieldnames=["id", "categoria", "entrada", "saida", "dock", "data"]
+    )
     writer.writeheader()
     writer.writerows(dados)
 
     output.seek(0)
-    csv_bytes = output.getvalue().encode('utf-8')
+    csv_bytes = output.getvalue().encode("utf-8")
     output.close()
 
-    data_primeira_entrada = dados[0]['data']
-    data_formatada = datetime.strptime(data_primeira_entrada, '%Y-%m-%d').strftime('%Y%m%d')
-    nome_arquivo = f'suape_vision_{data_formatada}.csv'
+    data_primeira_entrada = dados[0]["data"]
+    data_formatada = datetime.strptime(data_primeira_entrada, "%Y-%m-%d").strftime(
+        "%Y%m%d"
+    )
+    nome_arquivo = f"suape_vision_{data_formatada}.csv"
 
-    return send_file(io.BytesIO(csv_bytes), mimetype='text/csv', as_attachment=True, download_name=nome_arquivo)
+    return send_file(
+        io.BytesIO(csv_bytes),
+        mimetype="text/csv",
+        as_attachment=True,
+        download_name=nome_arquivo,
+    )
 
-@app.route('/api', methods=['GET'])
+
+@app.route("/api", methods=["GET"])
 def get_api_welcome():
-    return '''
+    return """
         <html>
             <head>
                 <title>API SUAPE VISION</title>
@@ -125,20 +189,22 @@ def get_api_welcome():
                 </div>
             </body>
         </html>
-    '''
+    """
 
-@app.route('/api/dados', methods=['GET'])
-@app.route('/api/dados/id/<id>', methods=['GET'])
-@app.route('/api/dados/data/<data>', methods=['GET'])
-@app.route('/api/dados/id/<id>/data/<data>', methods=['GET'])
+
+@app.route("/api/dados", methods=["GET"])
+@app.route("/api/dados/id/<id>", methods=["GET"])
+@app.route("/api/dados/data/<data>", methods=["GET"])
+@app.route("/api/dados/id/<id>/data/<data>", methods=["GET"])
 def get_dados(id=None, data=None):
     filtrados = dados
     if id:
-        filtrados = [item for item in filtrados if item['id'] == id]
+        filtrados = [item for item in filtrados if item["id"] == id]
     if data:
-        filtrados = [item for item in filtrados if item['data'] == data]
+        filtrados = [item for item in filtrados if item["data"] == data]
 
-    return jsonify(filtrados) if filtrados else ('', 404)
+    return jsonify(filtrados) if filtrados else ("", 404)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
